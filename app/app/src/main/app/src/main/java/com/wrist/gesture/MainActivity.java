6d6e172f8955eeeed231aadacadfae249617a17f
dixitjitekchand <dixitjitekchand@gmail.com>
2026-06-06 14:35:45 +0530

    Add MainActivity for Wrist Gesture App

---
+++ app/app/src/main/app/src/main/java/com/wrist/gesture/MainActivity.java
@@ -0,0 +1,16 @@
+package com.wrist.gesture;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setText("Wrist Gesture App Running");
+        setContentView(tv);
+    }
+}
